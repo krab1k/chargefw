cadf050fae265dd67bf51e988e3407c53668b1cb
Tomáš Raček <tom@krab1k.net>
2026-05-03 09:10:13 +0200

    Move defs into cpps

--- include/chargefw/methods/calculation_input.h
+++ include/chargefw/methods/calculation_input.h
@@ -15,9 +15,7 @@ class CalculationInput {
     CalculationInput(const features::PreparedMolecule& prepared_molecule,
                      const MethodOptions& method_options,
                      const features::ConformerFeatures* geometry = nullptr,
-                     const parameters::ParameterView* parameters = nullptr)
-        : prepared_molecule_{prepared_molecule}, method_options_{method_options},
-          geometry_{geometry}, parameters_{parameters} {}
+                     const parameters::ParameterView* parameters = nullptr);
 
     CalculationInput(features::PreparedMolecule&&, const MethodOptions&,
                      const features::ConformerFeatures* = nullptr,
@@ -27,55 +25,25 @@ class CalculationInput {
                      const features::ConformerFeatures* = nullptr,
                      const parameters::ParameterView* = nullptr) = delete;
 
-    [[nodiscard]] auto prepared_molecule() const noexcept -> const features::PreparedMolecule& {
-        return prepared_molecule_.get();
-    }
+    [[nodiscard]] auto prepared_molecule() const noexcept -> const features::PreparedMolecule&;
 
-    [[nodiscard]] auto molecule() const noexcept -> const core::Molecule& {
-        return prepared_molecule().molecule();
-    }
+    [[nodiscard]] auto molecule() const noexcept -> const core::Molecule&;
 
-    [[nodiscard]] auto topology() const noexcept -> const features::TopologyFeatures& {
-        return prepared_molecule().topology();
-    }
+    [[nodiscard]] auto topology() const noexcept -> const features::TopologyFeatures&;
 
-    [[nodiscard]] auto method_options() const noexcept -> const MethodOptions& {
-        return method_options_.get();
-    }
+    [[nodiscard]] auto method_options() const noexcept -> const MethodOptions&;
 
-    [[nodiscard]] auto has_geometry() const noexcept -> bool {
-        return geometry_ != nullptr;
-    }
+    [[nodiscard]] auto has_geometry() const noexcept -> bool;
 
-    [[nodiscard]] auto geometry_if_available() const noexcept
-        -> const features::ConformerFeatures* {
-        return geometry_;
-    }
+    [[nodiscard]] auto geometry_if_available() const noexcept -> const features::ConformerFeatures*;
 
-    [[nodiscard]] auto geometry() const -> const features::ConformerFeatures& {
-        if (geometry_ == nullptr) {
-            throw std::logic_error{"calculation input has no geometry"};
-        }
+    [[nodiscard]] auto geometry() const -> const features::ConformerFeatures&;
 
-        return *geometry_;
-    }
+    [[nodiscard]] auto has_parameters() const noexcept -> bool;
 
-    [[nodiscard]] auto has_parameters() const noexcept -> bool {
-        return parameters_ != nullptr;
-    }
+    [[nodiscard]] auto parameters_if_available() const noexcept -> const parameters::ParameterView*;
 
-    [[nodiscard]] auto parameters_if_available() const noexcept
-        -> const parameters::ParameterView* {
-        return parameters_;
-    }
-
-    [[nodiscard]] auto parameters() const -> const parameters::ParameterView& {
-        if (parameters_ == nullptr) {
-            throw std::logic_error{"calculation input has no parameters"};
-        }
-
-        return *parameters_;
-    }
+    [[nodiscard]] auto parameters() const -> const parameters::ParameterView&;
 
   private:
     std::reference_wrapper<const features::PreparedMolecule> prepared_molecule_;
---
+++ src/methods/calculation_input.cpp
@@ -0,0 +1,65 @@
+#include <chargefw/core/molecule.h>
+#include <chargefw/features/conformer_features.h>
+#include <chargefw/features/prepared_molecule.h>
+#include <chargefw/methods/calculation_input.h>
+#include <chargefw/methods/method_options.h>
+
+namespace chargefw::methods {
+
+CalculationInput::CalculationInput(const features::PreparedMolecule& prepared_molecule,
+                                   const MethodOptions& method_options,
+                                   const features::ConformerFeatures* geometry,
+                                   const parameters::ParameterView* parameters)
+    : prepared_molecule_{prepared_molecule}, method_options_{method_options}, geometry_{geometry},
+      parameters_{parameters} {}
+auto CalculationInput::prepared_molecule() const noexcept -> const features::PreparedMolecule& {
+    return prepared_molecule_.get();
+}
+
+auto CalculationInput::molecule() const noexcept -> const core::Molecule& {
+    return prepared_molecule().molecule();
+}
+
+auto CalculationInput::topology() const noexcept -> const features::TopologyFeatures& {
+    return prepared_molecule().topology();
+}
+
+auto CalculationInput::method_options() const noexcept -> const MethodOptions& {
+    return method_options_.get();
+}
+
+auto CalculationInput::has_geometry() const noexcept -> bool {
+    return geometry_ != nullptr;
+}
+
+auto CalculationInput::geometry_if_available() const noexcept
+    -> const features::ConformerFeatures* {
+    return geometry_;
+}
+
+auto CalculationInput::geometry() const -> const features::ConformerFeatures& {
+    if (geometry_ == nullptr) {
+        throw std::logic_error{"calculation input has no geometry"};
+    }
+
+    return *geometry_;
+}
+
+auto CalculationInput::has_parameters() const noexcept -> bool {
+    return parameters_ != nullptr;
+}
+
+auto CalculationInput::parameters_if_available() const noexcept
+    -> const parameters::ParameterView* {
+    return parameters_;
+}
+
+auto CalculationInput::parameters() const -> const parameters::ParameterView& {
+    if (parameters_ == nullptr) {
+        throw std::logic_error{"calculation input has no parameters"};
+    }
+
+    return *parameters_;
+}
+
+} // namespace chargefw::methods
