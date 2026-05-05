c8c1308bf6394d5bdbb2a57c06c8cb1fe244c56a
Tomáš Raček <tomas.racek@muni.cz>
2026-05-05 06:52:25 +0200

    Add internal BFS algorithm

---
+++ src/features/topology_helpers.cpp
@@ -0,0 +1,36 @@
+#include <optional>
+#include <queue>
+#include <utility>
+#include <vector>
+
+auto breadth_first_distances(const std::vector<std::vector<std::size_t>>& adjacency,
+                             const std::size_t source,
+                             const std::optional<std::size_t> max_distance) -> std::vector<int> {
+    auto distances = std::vector(adjacency.size(), -1);
+    auto queue = std::queue<std::size_t>{};
+
+    distances[source] = 0;
+    queue.push(source);
+
+    while (!queue.empty()) {
+        const auto current = queue.front();
+        queue.pop();
+
+        const auto current_distance = distances[current];
+
+        if (max_distance.has_value() && std::cmp_greater_equal(current_distance ,*max_distance)) {
+            continue;
+        }
+
+        for (const auto neighbor : adjacency[current]) {
+            if (distances[neighbor] != -1) {
+                continue;
+            }
+
+            distances[neighbor] = current_distance + 1;
+            queue.push(neighbor);
+        }
+    }
+
+    return distances;
+}
---
+++ src/features/topology_helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <optional>
+#include <vector>
+
+auto breadth_first_distances(const std::vector<std::vector<std::size_t>>& adjacency,
+                             std::size_t source, std::optional<std::size_t> max_distance)
+    -> std::vector<int>;
