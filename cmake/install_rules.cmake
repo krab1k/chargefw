9592dc6e0fe62149af0d58fb66dc26fcc7a4b3c7
Tomáš Raček <tomas.racek@muni.cz>
2026-05-05 08:51:44 +0200

    Install everything, not just binary

--- cmake/install_rules.cmake
+++ cmake/install_rules.cmake
@@ -1,15 +1,27 @@
 install(
-  DIRECTORY
-    ${PROJECT_SOURCE_DIR}/data/parameters
-  DESTINATION
-    ${CMAKE_INSTALL_DATADIR}/chargefw
-  FILES_MATCHING
-    PATTERN "*.json"
+        TARGETS chargefw_core
+        FILE_SET HEADERS
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+        ARCHIVE
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        LIBRARY
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME
+        DESTINATION ${CMAKE_INSTALL_BINDIR}
 )
 
 install(
-  FILES
-    ${PROJECT_BINARY_DIR}/generated/chargefw/config.h
-  DESTINATION
-    ${CMAKE_INSTALL_INCLUDEDIR}/chargefw
+        DIRECTORY
+        ${PROJECT_SOURCE_DIR}/data/parameters
+        DESTINATION
+        ${CMAKE_INSTALL_DATADIR}/chargefw
+        FILES_MATCHING
+        PATTERN "*.json"
+)
+
+install(
+        FILES
+        ${PROJECT_BINARY_DIR}/generated/chargefw/config.h
+        DESTINATION
+        ${CMAKE_INSTALL_INCLUDEDIR}/chargefw
 )
