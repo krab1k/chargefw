89a356ce5060c971e0adef89533bc0b66386b19c
Tomáš Raček <tomas.racek@muni.cz>
2026-05-05 13:46:39 +0200

    Prefer system deps

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,19 +1,23 @@
 include(FetchContent)
 
 function(chargefw_setup_dependencies)
-  FetchContent_Declare(
-    nlohmann_json
-    SYSTEM
-    URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz
-  )
+    find_package(nlohmann_json 3.12 CONFIG QUIET)
+    if(NOT TARGET nlohmann_json::nlohmann_json)
+        FetchContent_Declare(
+                nlohmann_json
+                SYSTEM
+                URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz
+        )
+        FetchContent_MakeAvailable(nlohmann_json)
+    endif()
 
-  FetchContent_MakeAvailable(nlohmann_json)
-
-  FetchContent_Declare(
-          eigen
-          SYSTEM
-          URL https://gitlab.com/libeigen/eigen/-/archive/5.0.1/eigen-5.0.1.tar.gz
-  )
-
-  FetchContent_MakeAvailable(eigen)
+    find_package(Eigen3 5.0 CONFIG QUIET)
+    if(NOT TARGET Eigen3::Eigen)
+        FetchContent_Declare(
+                eigen
+                SYSTEM
+                URL https://gitlab.com/libeigen/eigen/-/archive/5.0.1/eigen-5.0.1.tar.gz
+        )
+        FetchContent_MakeAvailable(eigen)
+    endif()
 endfunction()
