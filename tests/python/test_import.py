a9ef97667585e07b0daeba453c8a972d96072c49
Tomáš Raček <tomas.racek@muni.cz>
2026-08-27 10:17:14 +0200

    Make pytest compatible test

--- tests/python/test_import.py
+++ tests/python/test_import.py
@@ -1,6 +1,6 @@
 """Focused smoke test for the package skeleton."""
 
-import sys
+import os
 
 import chargefw
 import chargefw.calculation
@@ -9,54 +9,62 @@ import chargefw.methods
 import chargefw.parameters
 
 
-expected_version = sys.argv[1]
+expected_version = os.environ.get("CHARGEFW_EXPECTED_VERSION")
 
-assert chargefw.__version__ == expected_version
-assert chargefw.__all__ == [
-    "__version__",
-    "Molecule",
-    "MoleculeCollection",
-    "SourceIdentity",
-    "CalculationOptions",
-    "ChargeAssignment",
-    "CalculationResult",
-    "Assessment",
-    "Calculator",
-    "ChargeFWError",
-    "InvalidInputError",
-    "NoExecutablePlanError",
-    "NumericalFailureError",
-    "CalculationCancelledError",
-    "ExecutionSelectionKind",
-    "ExecutionMode",
-    "ChargeCorrectionPolicy",
-    "ExecutionStatus",
-    "PrerequisiteIssueKind",
-    "ExecutionAvailability",
-    "ExecutionIssueKind",
-    "PrerequisiteIssue",
-    "ExecutionIssue",
-    "ExecutionAssessment",
-    "ExecutionPolicy",
-    "ApplicableCandidate",
-    "RejectedCandidate",
-    "ApplicabilityReport",
-    "EffectiveCalculation",
-    "CalculationTimings",
-    "AssessmentReport",
-    "MethodOptionType",
-    "MethodOptionDescriptor",
-    "MethodDescriptor",
-    "method_descriptors",
-    "ParameterSet",
-    "ParameterSetDescriptor",
-    "load_parameter_set",
-    "load_parameter_sets",
-]
-assert chargefw.Molecule is chargefw.core.Molecule
-assert chargefw.CalculationOptions is chargefw.calculation.CalculationOptions
-assert chargefw.ExecutionIssue is chargefw.methods.ExecutionIssue
-assert chargefw.ExecutionMode.__module__ == "chargefw.calculation"
-assert chargefw.PrerequisiteIssueKind.__module__ == "chargefw.methods"
-assert chargefw.MethodOptionType.__module__ == "chargefw.methods"
-assert chargefw.ParameterSet is chargefw.parameters.ParameterSet
+def test_import_surface() -> None:
+    assert isinstance(chargefw.__version__, str)
+    assert chargefw.__version__
+    if expected_version is not None:
+        assert chargefw.__version__ == expected_version
+    assert chargefw.__all__ == [
+        "__version__",
+        "Molecule",
+        "MoleculeCollection",
+        "SourceIdentity",
+        "CalculationOptions",
+        "ChargeAssignment",
+        "CalculationResult",
+        "Assessment",
+        "Calculator",
+        "ChargeFWError",
+        "InvalidInputError",
+        "NoExecutablePlanError",
+        "NumericalFailureError",
+        "CalculationCancelledError",
+        "ExecutionSelectionKind",
+        "ExecutionMode",
+        "ChargeCorrectionPolicy",
+        "ExecutionStatus",
+        "PrerequisiteIssueKind",
+        "ExecutionAvailability",
+        "ExecutionIssueKind",
+        "PrerequisiteIssue",
+        "ExecutionIssue",
+        "ExecutionAssessment",
+        "ExecutionPolicy",
+        "ApplicableCandidate",
+        "RejectedCandidate",
+        "ApplicabilityReport",
+        "EffectiveCalculation",
+        "CalculationTimings",
+        "AssessmentReport",
+        "MethodOptionType",
+        "MethodOptionDescriptor",
+        "MethodDescriptor",
+        "method_descriptors",
+        "ParameterSet",
+        "ParameterSetDescriptor",
+        "load_parameter_set",
+        "load_parameter_sets",
+    ]
+    assert chargefw.Molecule is chargefw.core.Molecule
+    assert chargefw.CalculationOptions is chargefw.calculation.CalculationOptions
+    assert chargefw.ExecutionIssue is chargefw.methods.ExecutionIssue
+    assert chargefw.ExecutionMode.__module__ == "chargefw.calculation"
+    assert chargefw.PrerequisiteIssueKind.__module__ == "chargefw.methods"
+    assert chargefw.MethodOptionType.__module__ == "chargefw.methods"
+    assert chargefw.ParameterSet is chargefw.parameters.ParameterSet
+
+
+if __name__ == "__main__":
+    test_import_surface()
